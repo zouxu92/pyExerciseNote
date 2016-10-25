80f27dbe076667a1ddecc18aacb8a6caa9c7cc2e
zouxu_PC <zouxu_PC>
2016-10-25 17:27:11 +0800

    将练习一添加到远程仓库

---
+++ 0000/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/evn python
+# excoding: utf-8
+
+import Image
+import ImageFont
+import ImageDraw
+
+text = u"7"
+
+im = Image.open('./image.jpg')
+
+dr = ImageDraw.Draw(im)
+font = ImageFont.truetype("msyh.ttf", 34)
+
+dr.text(im.size[0]*0.85, im.size[1]*0.05, text, font=font, fill="#ff0000")
+
+im.show()
+im.save('result.jpg')
