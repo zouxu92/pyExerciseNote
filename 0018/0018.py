0ea598e346d0bd9173dbdb13d944fac281d776aa
zouxu_PC <zouxu_PC>
2016-11-09 16:04:41 +0800

    添加将字典的xls读取后写入到xml文件--特殊格式利用了replace函数处理

---
+++ 0018/0018.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+'''将 第 0015 题中的 city.xls 文件中的内容写到 city.xml 文件中'''
+import xlrd
+import json
+from collections import OrderedDict
+import xml.dom.minidom as minidom
+import HTMLParser
+
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def get_xls_data(fileName):
+	book = xlrd.open_workbook(fileName)  # 获取对象
+	sheet = book.sheet_by_index(0)      # 获取工作目录
+	d = OrderedDict()
+	for i in range(sheet.nrows):
+		# 读一行，存放到dict中
+		values = map(lambda x: x.encode('UTF8') if isinstance(x, unicode) else x, sheet.row_values(i))
+		d[values[0]] = values[1]
+	return d #json.dumps(d, indent=4,ensure_ascii=False, encoding='utf-8').decode('utf-8')
+
+# 将字典里面的数据装换为正确的中文
+
+json_data = json.dumps(get_xls_data('city.xls'), indent=4,ensure_ascii=False, encoding='utf-8')
+print json_data.decode('utf-8')
+
+def writeXml(xlscontent):
+	xmlfile = minidom.Document()  # 创建新xml文件
+	root = xmlfile.createElement('root') # 创建节点
+	citys = xmlfile.createElement ( 'citys' ) # 节点二
+
+	xmlfile.appendChild(root) # 在文件中添加root节点
+	root.appendChild(citys) # root下的节点
+
+	# 创建注释
+	comment = xmlfile.createComment('城市信息')
+
+	citys.appendChild(comment) # 在students 标签下添加comment
+
+	html_parser = HTMLParser.HTMLParser()
+	tranform = html_parser.unescape(json_data.toxml().decode('utf-8'))
+	print tranform
+	xmlcontent = xmlfile.createTextNode(r'%s' % tranform.decode('utf-8'))#str(xlscontent).decode('utf-8')) # 创建文本节点
+	citys.appendChild(xmlcontent) # 在students标签下添加文本内容
+	# 写入文件
+	with open('city.xml', 'w') as f:
+
+        # f.write(tranform.encode('utf-8'))
+
+		f.write(xmlfile.toprettyxml(newl = '\n', encoding = 'UTF-8'))
+		f.close # 关闭文件
+
+if __name__ == "__main__":
+	writeXml(get_xls_data('city.xls'))
+
